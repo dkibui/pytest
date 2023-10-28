e9320a62d186f530cec9c5269b27476a080e40ad
David Kibui <dkibui@outlook.com>
2023-10-28 22:16:15 +0300

    Getting started with pytest testing

---
+++ 01/test_sample.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    assert func(3) == 4
+
+
+# ======================================================
+def f():
+    raise SystemExit(1)
+
+
+def test_f():
+    with pytest.raises(SystemExit):
+        f()
